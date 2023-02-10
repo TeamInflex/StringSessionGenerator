d08a3fd86705e47a49cbe68681a02cfd6042ff19
Team Inflex <TheTeamInflex@Gmail.com>
2023-02-10 21:01:54 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -14,17 +14,23 @@ async def start(bot: Client, msg: Message):
         chat_id=msg.chat.id,
         text=f"""Hᴇʏ {msg.from_user.mention},
 
-Tʜɪs ɪs {me2},
-Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+Wᴇʟᴄᴏᴍᴇ Tᴏ {me2},
 
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ɪɴꜰʟᴇx](tg://user?id={OWNER_ID}) !""",
+ɪꜰ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴛʀᴜꜱᴛ ᴛʜɪꜱ ʙᴏᴛ ,
+1. ꜱᴛᴏᴘ ʀᴇᴀᴅɪɴɢ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ
+2. ᴅᴇʟᴇᴛᴇ ᴛʜɪꜱ ᴄʜᴀᴛ
+
+ꜱᴛɪʟʟ ʀᴇᴀᴅɪɴɢ ?
+ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ( ᴇᴠᴇɴ ᴠᴇʀꜱɪᴏɴ 2 ) ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ. ᴜꜱᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ᴛᴏ ʟᴇᴀʀɴ ᴍᴏʀᴇ !
+
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [@InflexUpdates](https://t.me/InflexUpdates) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="🔥 Sᴛᴀʀᴛ Gᴇɴᴇʀᴀᴛɪɴɢ Sᴇꜱꜱɪᴏɴ 🔥", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❤️ Sᴜᴘᴘᴏʀᴛ ❤️", url="https://t.me/InflexSupport"),
+                    InlineKeyboardButton("❤️ Uᴘᴅᴀᴛᴇꜱ ❤️", url="https://t.me/InflexUpdates"),
                     InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID)
                 ]
             ]
